118080c1cd5952c3e7e8fe831389ee9dd2445b37
SodikulPadli <sodikulfadly91@gmail.com>
2022-02-10 11:20:41 +0800

    melakukan manipulasi array

--- index.js
+++ index.js
@@ -19,9 +19,21 @@ app.use(express.urlencoded({ extended: false }));
 
 const isLogin = true;
 
+// kondisi default
+const blogs = [
+  {
+    title: 'Pasar Coding di Indonesia Dinilai Masih Menjanjikan',
+    content: 'Ketimpangan sumber daya manusia (SDM) di sektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup, ketimpangan SDM global, termasuk Indonesia, meningkat dua kali lipat dalam satu dekade',
+    author: 'Ichsan Emrald Alamsyah',
+    posted_at: '12 Jul 2021 22:30 WIB ',
+  },
+];
+
+let month = ['january', 'February', 'Maret', 'April', 'Mei', 'Juni', 'July', 'August', 'September', 'October', 'November', 'Desember'];
+
 // set endpoint
 app.get('/', function (request, response) {
-  response.send('Hello Word');
+  response.send('index');
 });
 
 app.get('/home', function (request, response) {
@@ -33,7 +45,16 @@ app.get('/contact-me', function (request, response) {
 });
 
 app.get('/blog', function (request, response) {
-  response.render('blog', { isLogin: isLogin });
+  console.log(blogs);
+  // manipulsai data string object
+  let dataBlogs = blogs.map(function (data) {
+    return {
+      // spread operator untuk melakukan manipulasi
+      ...data,
+      isLogin: isLogin,
+    };
+  });
+  response.render('blog', { isLogin: isLogin, blogs: dataBlogs });
 });
 
 app.get('/add-blog', function (request, response) {
@@ -45,14 +66,44 @@ app.get('/blog/:id', function (request, response) {
   let id = request.params.id;
   console.log(`id dari client : ${id}`);
 
+  // jika ingin mengirimkan data yang mempunyai nilai
   response.render('blog-detail', { identity: id });
+});
+
+// delete data
+app.get('/delete-blog/:index', function (request, response) {
+  let index = request.params.index;
+  console.log(`index delete ${index}`);
+
+  blogs.splice(index, 1);
+  response.redirect('/blog');
+});
+
+// update data
+app.get('/from-blog/:index', function (request, response) {
+  let index = request.params.index;
+  console.log(`index update ${index}`);
+  response.render('form-blog', { index: index });
 });
 
 //  jika data dikirimkan dari formulir maka gunakan body
 // mengirimkan data mengunakan formulir pastikan element dari tiap inputan harus ada
 app.post('/blog', function (request, response) {
   let title = request.body.title;
-  console.log(`data titile `);
+  let content = request.body.content;
+
+  let blog = {
+    title: title,
+    content,
+    author: 'Sodikul Padli',
+    posted_at: getFullTime(new Date()),
+  };
+
+  // memasukan data
+  blogs.push(blog);
+
+  // jika tidak ada data/nilai yang dikirimkan
+  response.redirect('/blog');
 });
 // konfigurasi port aplikasi
 
@@ -60,3 +111,16 @@ const port = 5000;
 app.listen(port, function () {
   console.log('Server Running On');
 });
+
+// menampilkan waktu dan tanggal terkini
+function getFullTime(time) {
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  return `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`;
+}
